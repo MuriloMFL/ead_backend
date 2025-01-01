534aca0d31677a1b4121ddb7bcdeef66d6cc2010
MuriloMFL <mumancha0@gmail.com>
2025-01-01 06:12:42 -0300

    Update servicoAtualizarUsuario.ts

--- src/servico/usuario/servicoAtualizarUsuario.ts
+++ src/servico/usuario/servicoAtualizarUsuario.ts
@@ -4,17 +4,22 @@ import { hash } from 'bcryptjs';
 class ServicoAtualizarUsuario {
     async executar ({id_usuario, nome_usuario, email, login, senha, id_franquia, tipo_usuario}){
 
-        const passwordHash = await hash(senha, 8);
+        const dadosAtualizacao: any = {
+            nome_usuario,
+            email,
+            login,
+            id_franquia: Number(id_franquia),
+            tipo_usuario
+        }
+
+        if(senha) {
+            const passwordHash = await hash(senha, 8);
+            dadosAtualizacao.senha = passwordHash;
+        }
 
         const usuario = await prisma.usuario.update({
-            data: {
-                nome_usuario: nome_usuario,
-                email: email,
-                login: login,
-                senha: passwordHash,
-                id_franquia: Number(id_franquia),
-                tipo_usuario: tipo_usuario
-            }, where:{
+            data: dadosAtualizacao,
+            where:{
                 id_usuario : Number(id_usuario)
             }
         });
