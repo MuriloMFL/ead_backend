e24c16bd7f6c54894fe8d058d6f1bb37f6eb159b
MuriloMFL <mumancha0@gmail.com>
2025-01-02 06:57:44 -0300

    0201manha

--- src/servico/planejamento/servicoAtualizarPlanejamento.ts
+++ src/servico/planejamento/servicoAtualizarPlanejamento.ts
@@ -21,24 +21,6 @@ class ServicoAtualizarPlanejamento {
         impedimentos,
         observacoes
     }){
-        console.log( id_planejamento,       
-            nome_planejamento,
-            id_sistema,
-            id_modulo,
-            id_submodulo,
-            id_usuario,
-            modulo_novo,
-            previsao_inicio,
-            previsao_fim,
-            etapa_elicitacao,
-            etapa_roteiro,
-            etapa_video,
-            etapa_validacao,
-            etapa_finalizado,
-            proposta,
-            roteiro,
-            impedimentos,
-            observacoes)
         try {
             const planejamento = await prismaClient.planejamento.update({
                 data: {
