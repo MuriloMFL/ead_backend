be543b323bf13e7450c3557de64956e372df9ac0
MuriloMFL <mumancha0@gmail.com>
2025-01-13 06:50:55 -0300

    13

--- src/controle/prova/controleListarProva.ts
+++ src/controle/prova/controleListarProva.ts
@@ -3,9 +3,7 @@ import { Response, Request } from "express";
 
 class ControleListarProva {
     async handle(req:Request, res: Response){
-        const {id_prova, id_sistema, nome_prova, id_modulo, id_submodulo, status, id_usuario} = req.query;
-        
-        console.log(id_prova, id_sistema, nome_prova, id_modulo, id_submodulo, status, id_usuario)
+        const {id_prova, id_sistema, nome_prova, id_modulo, id_submodulo, status} = req.query;
         
         const filtros = {
             id_prova     : id_prova,
@@ -14,12 +12,11 @@ class ControleListarProva {
             id_submodulo : id_submodulo,
             nome_prova   : nome_prova,
             status       : status == 'true' ? true : status =='false' ? false : undefined,
-            id_usuario   : Number(id_usuario)
+
         }
-
-        
+        console.log(id_prova, id_sistema, nome_prova, id_modulo, id_submodulo, status)
         const servicolistarprovas = new ServicoListarProva();
-
+        
         const provas = await servicolistarprovas.executar(filtros)
 
         return res.json(provas)
--- src/servico/prova/servicoListarProva.ts
+++ src/servico/prova/servicoListarProva.ts
@@ -3,42 +3,42 @@ import prismaClient from "../../prisma";
 
 class ServicoListarProva {
     async executar(filtros){
-        
-        const dados = {
-            id_prova     : filtros.id_prova,
-            id_sistema   : filtros.id_sistema,
-            id_modulo    : filtros.id_modulo,
-            id_submodulo : filtros.id_submodulo,
-            nome_prova   : filtros.nome_prova ? {contains: filtros.nome_prova, mode: Prisma.QueryMode.insensitive} : undefined,
-            status       : filtros.status,
-        }
 
-        const buscaprova = await prismaClient.prova.findMany({
-            where: dados,
-            include: {
-              sistema: {
-                select: { nome_sistema: true },
-              },
-              modulo: {
-                select: { nome_modulo: true },
-              },
-              submodulo: {
-                select: { nome_submodulo: true },
-              },
+      const dados = {
+          id_prova     : filtros.id_prova,
+          id_sistema   : filtros.id_sistema,
+          id_modulo    : filtros.id_modulo,
+          id_submodulo : filtros.id_submodulo,
+          nome_prova   : filtros.nome_prova ? {contains: filtros.nome_prova, mode: Prisma.QueryMode.insensitive} : undefined,
+          status       : filtros.status,
+      }
+
+      const buscaprova = await prismaClient.prova.findMany({
+          where: dados,
+          include: {
+            sistema: {
+              select: { nome_sistema: true },
             },
-          });
+            modulo: {
+              select: { nome_modulo: true },
+            },
+            submodulo: {
+              select: { nome_submodulo: true },
+            },
+          },
+        });
                
-          return buscaprova.map((prova) => ({
-            id_prova: prova.id_prova,
-            id_sistema: prova.id_sistema,
-            nome_sistema: prova.sistema?.nome_sistema,
-            id_modulo: prova.id_modulo,
-            nome_modulo: prova.modulo?.nome_modulo,
-            id_submodulo: prova.id_submodulo,
-            nome_submodulo: prova.submodulo?.nome_submodulo,
-            nome_prova: prova.nome_prova,
-            status: prova.status,
-          }));         
+        return buscaprova.map((prova) => ({
+          id_prova: prova.id_prova,
+          id_sistema: prova.id_sistema,
+          nome_sistema: prova.sistema?.nome_sistema,
+          id_modulo: prova.id_modulo,
+          nome_modulo: prova.modulo?.nome_modulo,
+          id_submodulo: prova.id_submodulo,
+          nome_submodulo: prova.submodulo?.nome_submodulo,
+          nome_prova: prova.nome_prova,
+          status: prova.status,
+        }));         
     }
 }
 export {ServicoListarProva}
