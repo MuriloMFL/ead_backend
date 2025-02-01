19a950a39305d45c2f13d8e91c2237786d61b592
MuriloMFL <mumancha0@gmail.com>
2025-02-01 06:46:47 -0300

    a

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json()); 
 
 app.use(cors({
-    origin: ['https://ead.murilofrizziero.dev.br', 'http://localhost:3000'],
+    origin: '*',  
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
